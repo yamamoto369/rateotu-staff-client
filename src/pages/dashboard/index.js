9df578ec73c8dc4439b114ad3be621805207b807
Edin <yamamoto.nakamoto@gmail.com>
2022-05-01 22:22:52 +0200

    Added employee main dashboard page component

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,32 +1,14 @@
 import React from 'react';
-import Loader from 'components/LayoutComponents/Loader';
 
-class MenuList extends React.Component {
-  state = {
-    isLoading: false,
-  };
-
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  fetchData = () => {
-  };
-
-
-  render() {
-    const { isLoading } = this.state;
-
-    if (isLoading) {
-      return <Loader />;
-    }
-
-    return (
-      <>
-        <p>Here we go Here we go Here we go again...</p>
-      </>
-    );
-  }
+function DashboardMain() {
+  return (
+    <h1>
+      Main Restaurant Dashboard
+      <span role="img" aria-label="spark">
+        ✨
+      </span>
+    </h1>
+  );
 }
 
-export default MenuList;
+export default DashboardMain;
