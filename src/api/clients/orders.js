ee142b063b64d4115c1dbd193c6aa64e44a73aa7
unknown <tiroedin@gmail.com>
2022-05-01 16:46:23 +0200

    Added new relative API paths and methods for OrderAPIClient

--- src/api/clients/orders.js
+++ src/api/clients/orders.js
@@ -2,6 +2,10 @@ import APIClient from 'api/clients/base';
 import { buildUrl } from 'utils/api';
 
 const paths = {
+  ORDERS: 'orders',
+  ORDER_ITEMS: 'orders/items',
+  ORDER_ITEM: 'orders/items/:id',
+  ORDER_ITEMS_BULK_UPDATE: 'orders/items/bulk-update',
 };
 
 class OrderAPIClient {
@@ -9,6 +13,21 @@ class OrderAPIClient {
     this.client = new APIClient();
   }
 
+  getOrders(queryParams = {}) {
+    return this.client.get(buildUrl(paths.ORDERS, { queryParams }));
+  }
+
+  getOrderItems(queryParams = {}) {
+    return this.client.get(buildUrl(paths.ORDER_ITEMS, { queryParams }));
+  }
+
+  partialyOrderItem(pathParams, requestBody) {
+    return this.client.patch(buildUrl(paths.ORDER_ITEM, { pathParams }), requestBody);
+  }
+
+  bulkUpdateOrderItemsOrderStatus(requestBody) {
+    return this.client.patch(buildUrl(paths.ORDER_ITEMS_BULK_UPDATE), requestBody);
+  }
 }
 
 export default new OrderAPIClient();
