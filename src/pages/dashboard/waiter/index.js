1078e8989df0f6cf3498f3449a58723dddc69ac4
Edin <yamamoto.nakamoto@gmail.com>
2022-05-08 16:05:29 +0200

    Added table column and column search functionality to employee page components

--- src/pages/dashboard/waiter/index.js
+++ src/pages/dashboard/waiter/index.js
@@ -1,110 +1,16 @@
 import React from 'react';
-import { Button, message, Tooltip, Icon, notification } from 'antd';
+import { Button, Input, message, Tooltip, Icon, notification } from 'antd';
 import { connect } from 'react-redux';
 import Loader from 'components/LayoutComponents/Loader';
 import OrderAPIClient from 'api/clients/orders';
 import OrderItemTable from 'components/RateotuComponents/OrderItemTable';
 import moment from 'moment';
 import camelcaseKeys from 'camelcase-keys';
+import Highlighter from 'react-highlight-words';
 import { renderPaymentStatusTag, renderItemTypeTag, renderOrderStatusTag } from 'utils/ui';
 import styles from '../style.module.scss';
 
 // TODO: Make it DRY
-
-const columns = [
-  {
-    title: 'ID',
-    dataIndex: 'id',
-    sorter: (a, b) => a.id - b.id,
-  },
-  {
-    title: 'Order ID',
-    dataIndex: 'order.id',
-    sorter: (a, b) => a.order.id - b.order.id,
-  },
-  {
-    title: 'Name',
-    dataIndex: 'item.name',
-    sorter: (a, b) => a.item.name.length - b.item.name.length,
-  },
-  {
-    title: 'Quantity',
-    dataIndex: 'quantity',
-    sorter: (a, b) => a.quantity - b.quantity,
-  },
-  {
-    title: 'Price',
-    dataIndex: 'price',
-    sorter: (a, b) => b.price - b.price,
-  },
-  {
-    title: 'Category',
-    dataIndex: 'item.category.name',
-    sorter: (a, b) => a.item.category.name.length - b.item.category.name.length,
-    render: (value) => renderItemTypeTag(value),
-  },
-  {
-    title: () => (
-      <span className="d-inline-block">
-        <Tooltip
-          className="align-self-center mr-1"
-          title="Showing only items with ready and serving order status."
-        >
-          <Icon type="info-circle" />
-        </Tooltip>
-        Order status
-      </span>
-    ),
-    dataIndex: 'orderStatus',
-    filters: [
-      { text: 'Created', value: 'created' },
-      { text: 'Preparing', value: 'preparing' },
-    ],
-    onFilter: (value, row) => row.orderStatus === value,
-    sorter: (a, b) => a.orderStatus.length - b.orderStatus.length,
-    render: (value) => renderOrderStatusTag(value),
-  },
-  {
-    title: 'Payment status',
-    dataIndex: 'paymentStatus',
-    filters: [
-      { text: 'Pending', value: 'pending' },
-      { text: 'Paid', value: 'paid' },
-      { text: 'Not paid', value: 'not paid' },
-    ],
-    onFilter: (value, row) => row.paymentStatus === value,
-    sorter: (a, b) => a.paymentStatus.length - b.paymentStatus.length,
-    render: (value) => renderPaymentStatusTag(value),
-  },
-  {
-    title: 'Created',
-    dataIndex: 'createdAt',
-    defaultSortOrder: 'descend',
-    sorter: (a, b) => moment(a.createdAt).unix() - moment(b.createdAt).unix(),
-    render: (value) => moment(value).format('DD-MM-YYYY HH:mm:ss'),
-  },
-  {
-    title: 'Updated',
-    dataIndex: 'updatedAt',
-    defaultSortOrder: 'descend',
-    sorter: (a, b) => moment(a.createdAt).unix() - moment(b.createdAt).unix(),
-    render: (value) => moment(value).format('DD-MM-YYYY HH:mm:ss'),
-  },
-  {
-    title: 'Image',
-    dataIndex: 'item.imageUrl',
-    render: (value) => (
-      <a href="#" className={styles.thumbnail}>
-        <img src={value} alt="" />
-      </a>
-    ),
-  },
-  {
-    title: 'Customer',
-    dataIndex: 'customer.username',
-    sorter: (a, b) => a.customer.username.length - b.customer.username.length,
-  },
-];
 
 const mapStateToProps = (state) => {
   return {
@@ -153,6 +59,69 @@ class DashboardWaiter extends React.Component {
         };
       });
     });
+  };
+
+  getColumnSearchProps = (dataIndex, columnTitle) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={(node) => {
+            this.searchInput = node;
+          }}
+          placeholder={`Search ${columnTitle}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Button
+          type="primary"
+          onClick={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+          icon="search"
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: (visible) => {
+      if (visible) {
+        setTimeout(() => this.searchInput.select());
+      }
+    },
+    render: (text) =>
+      this.state.searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[this.state.searchText]}
+          autoEscape
+          textToHighlight={text.toString()}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    this.setState({
+      searchText: selectedKeys[0],
+      searchedColumn: dataIndex,
+    });
+  };
+
+  handleReset = (clearFilters) => {
+    clearFilters();
+    this.setState({ searchText: '' });
   };
 
   handleSelectChange = (selectedRowKeys) => {
@@ -262,6 +231,115 @@ class DashboardWaiter extends React.Component {
       onChange: this.handleSelectChange,
     };
     const hasSelected = selectedRowKeys.length > 0;
+
+    const columns = [
+      {
+        title: 'ID',
+        dataIndex: 'id',
+        sorter: (a, b) => a.id - b.id,
+        ...this.getColumnSearchProps('id', 'ID'),
+      },
+      {
+        title: 'Order ID',
+        dataIndex: 'order.id',
+        sorter: (a, b) => a.order.id - b.order.id,
+      },
+      {
+        title: 'Table',
+        dataIndex: 'table.tableNumber',
+        filters: [
+          { text: 'a1', value: 'a1' },
+          { text: 'a2', value: 'a2' },
+          { text: 'a3', value: 'a3' },
+        ],
+        onFilter: (value, row) => row.table.tableNumber === value,
+        sorter: (a, b) => a.table.tableNumber.length - b.table.tableNumber.length,
+      },
+      {
+        title: 'Name',
+        dataIndex: 'item.name',
+        sorter: (a, b) => a.item.name.length - b.item.name.length,
+      },
+      {
+        title: 'Quantity',
+        dataIndex: 'quantity',
+        sorter: (a, b) => a.quantity - b.quantity,
+        ...this.getColumnSearchProps('quantity', 'Quantity'),
+      },
+      {
+        title: 'Price',
+        dataIndex: 'price',
+        sorter: (a, b) => b.price - b.price,
+        ...this.getColumnSearchProps('price', 'Price'),
+      },
+      {
+        title: 'Category',
+        dataIndex: 'item.category.name',
+        sorter: (a, b) => a.item.category.name.length - b.item.category.name.length,
+        render: (value) => renderItemTypeTag(value),
+      },
+      {
+        title: () => (
+          <span className="d-inline-block">
+            <Tooltip
+              className="align-self-center mr-1"
+              title="Showing only items with ready and serving order status."
+            >
+              <Icon type="info-circle" />
+            </Tooltip>
+            Order status
+          </span>
+        ),
+        dataIndex: 'orderStatus',
+        filters: [
+          { text: 'Created', value: 'created' },
+          { text: 'Preparing', value: 'preparing' },
+        ],
+        onFilter: (value, row) => row.orderStatus === value,
+        sorter: (a, b) => a.orderStatus.length - b.orderStatus.length,
+        render: (value) => renderOrderStatusTag(value),
+      },
+      {
+        title: 'Payment status',
+        dataIndex: 'paymentStatus',
+        filters: [
+          { text: 'Pending', value: 'pending' },
+          { text: 'Paid', value: 'paid' },
+          { text: 'Not paid', value: 'not paid' },
+        ],
+        onFilter: (value, row) => row.paymentStatus === value,
+        sorter: (a, b) => a.paymentStatus.length - b.paymentStatus.length,
+        render: (value) => renderPaymentStatusTag(value),
+      },
+      {
+        title: 'Created',
+        dataIndex: 'createdAt',
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => moment(a.createdAt).unix() - moment(b.createdAt).unix(),
+        render: (value) => moment(value).format('DD-MM-YYYY HH:mm:ss'),
+      },
+      {
+        title: 'Updated',
+        dataIndex: 'updatedAt',
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => moment(a.createdAt).unix() - moment(b.createdAt).unix(),
+        render: (value) => moment(value).format('DD-MM-YYYY HH:mm:ss'),
+      },
+      {
+        title: 'Image',
+        dataIndex: 'item.imageUrl',
+        render: (value) => (
+          <a href="#" className={styles.thumbnail}>
+            <img src={value} alt="" />
+          </a>
+        ),
+      },
+      {
+        title: 'Customer',
+        dataIndex: 'customer.username',
+        sorter: (a, b) => a.customer.username.length - b.customer.username.length,
+      },
+    ];
 
     if (isLoading.page) {
       return <Loader />;
